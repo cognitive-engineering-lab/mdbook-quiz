5cfa431b45a26f6520e88796367e6969738e9f5c
Will Crichton <wcrichto@cs.stanford.edu>
2022-06-22 20:12:11 -0700

    Add build script

---
+++ build.rs
@@ -0,0 +1,27 @@
+use std::{path::Path, process::Command};
+
+const JS_DIST_DIR: &str = "js/dist";
+
+fn main() {
+  if !Path::new(JS_DIST_DIR).exists() {
+    let status = Command::new("pnpm")
+      .current_dir("js")
+      .arg("install")
+      .status()
+      .unwrap();
+    if !status.success() {
+      panic!("Failed to install JS deps")
+    }
+
+    let status = Command::new("pnpm")
+      .current_dir("js")
+      .arg("build")
+      .status()
+      .unwrap();
+    if !status.success() {
+      panic!("Failed to build JS");
+    }
+
+    return;
+  }
+}
