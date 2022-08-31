9b74db930e35c4a81e22bb20eb9bbb333299bd5f
Will Crichton <wcrichto@cs.stanford.edu>
2022-08-31 10:24:41 -0700

    Fix line number insertion

--- js/lib/components/snippet.tsx
+++ js/lib/components/snippet.tsx
@@ -1,36 +1,5 @@
 import React, { useEffect, useRef } from "react";
 
-let splitHljsOutput = (root: Node): Node[] => {
-  let lines: Node[][] = [[]];
-  let curLine = () => lines[lines.length - 1];
-  root.childNodes.forEach(child => {
-    if (child.nodeType == Node.TEXT_NODE) {
-      let content = child.textContent!;
-      let childLines = content.split("\n");
-      if (childLines.length > 1) {
-        curLine().push(document.createTextNode(childLines[0]));
-
-        childLines.slice(1, -1).forEach(childLine => {
-          lines.push([document.createTextNode(childLine)]);
-        });
-
-        lines.push([document.createTextNode(childLines[childLines.length - 1])]);
-      } else {
-        curLine().push(document.createTextNode(content));
-      }
-    } else {
-      curLine().push(child);
-    }
-  });
-
-  return lines.map(line => {
-    let span = document.createElement("span");
-    line.forEach(node => {
-      span.appendChild(node);
-    });
-    return span;
-  });
-};
 export let snippetToNode = (snippet: string, language?: string): HTMLPreElement => {
   let hljs = (window as any).hljs;
 
@@ -40,8 +9,14 @@ export let snippetToNode = (snippet: string, language?: string): HTMLPreElement 
   // use `[]` to delimit <mark> regions
   snippet = snippet.replace(/`\[/g, "<mark>").replace(/\]`/g, "</mark>");
 
+  // Insert line numbers for multi-line snippets
+  if (snippet.includes("\n")) {
+    let lines = snippet.split("\n");
+    snippet = lines.map(line => `<span class="line-number"></span><code>${line}</code>`).join("\n");
+  }
+
   let code = document.createElement("code");
-  code.className = "language-rust";
+  code.className = `language-${language || "rust"}`;
   code.innerHTML = snippet;
 
   // goddamn hack for esbuild-jest
@@ -49,24 +24,7 @@ export let snippetToNode = (snippet: string, language?: string): HTMLPreElement 
   f(code, language);
 
   let pre = document.createElement("pre");
-  let lines = splitHljsOutput(code);
-  if (lines.length > 1) {
-    let wrapped = lines
-      .map(line => {
-        let codeEl = document.createElement("code");
-        codeEl.appendChild(line);
-        let numEl = document.createElement("span");
-        numEl.className = "line-number";
-        return [numEl, codeEl];
-      })
-      .flat();
-    wrapped.forEach(node => {
-      pre.appendChild(node);
-    });
-  } else if (lines.length == 1) {
-    pre.appendChild(lines[0]);
-  }
-
+  pre.innerHTML = code.innerHTML;
   return pre;
 };
 
