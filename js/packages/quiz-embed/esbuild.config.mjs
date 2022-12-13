36f465a3adb4563617beeca934a712c8a12643a8
Will Crichton <wcrichto@cs.stanford.edu>
2022-12-12 21:11:56 -0800

    Fix rust-editor support

--- js/packages/quiz-embed/esbuild.config.mjs
+++ js/packages/quiz-embed/esbuild.config.mjs
@@ -1,3 +1,34 @@
+import fs from "fs";
+import path from "path";
+
+let rustEditorPlugin = {
+  name: "rust-editor",
+  setup(build) {
+    build.onEnd(() => {
+      if (process.env.RUST_EDITOR === undefined) return;
+
+      let files = ["editor.worker.js", "ra-worker.js", "wasm_demo_bg.wasm"];
+      files.forEach(f =>
+        fs.copyFileSync(
+          path.join("node_modules/@wcrichto/rust-editor/dist", f),
+          path.join("dist", f)
+        )
+      );
+
+      ["ra-worker"].forEach(name => {
+        let assetPath = path.join("dist", `${name}.js`);
+        let contents = fs.readFileSync(assetPath, "utf-8");
+        contents = contents.replace(
+          /import\.meta\.url/g,
+          `"http://localhost:3000/mdbook-quiz/"`
+        );
+        fs.writeFileSync(assetPath, contents);
+      });
+    });
+  },
+};
+
 export default {
-  format: "iife"
-}
+  format: "iife",
+  plugins: [rustEditorPlugin],
+};
--- js/packages/quiz/esbuild.config.mjs
+++ js/packages/quiz/esbuild.config.mjs
@@ -1,36 +1,15 @@
 let rustEditorPlugin = {
   name: "rust-editor",
   setup(build) {
-    if (process.env.RUST_EDITOR === undefined) {
-      build.onResolve({ filter: /@wcrichto\/rust-editor/ }, args => ({
-        path: args.path,
-        namespace: "rust-editor",
-      }));
-      build.onLoad({ filter: /.*/, namespace: "rust-editor" }, () => ({
-        contents: "module.exports = {}",
-      }));
-      return;
-    }
+    if (process.env.RUST_EDITOR !== undefined) return;
 
-    build.onEnd(() => {
-      let files = ["editor.worker.js", "ra-worker.js", "wasm_demo_bg.wasm"];
-      files.forEach(f =>
-        fs.copyFileSync(
-          path.join("node_modules/@wcrichto/rust-editor/dist", f),
-          path.join("dist", f)
-        )
-      );
-
-      ["ra-worker"].forEach(name => {
-        let assetPath = path.join("dist", `${name}.js`);
-        let contents = fs.readFileSync(assetPath, "utf-8");
-        contents = contents.replace(
-          /import\.meta\.url/g,
-          `"http://localhost:3000/mdbook-quiz/"`
-        );
-        fs.writeFileSync(assetPath, contents);
-      });
-    });
+    build.onResolve({ filter: /@wcrichto\/rust-editor/ }, args => ({
+      path: args.path,
+      namespace: "rust-editor",
+    }));
+    build.onLoad({ filter: /.*/, namespace: "rust-editor" }, () => ({
+      contents: "module.exports = {}",
+    }));
   },
 };
 
