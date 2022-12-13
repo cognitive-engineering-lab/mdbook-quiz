ba79d9a92754d56aac04f521cb20604e8458494a
Will Crichton <wcrichto@cs.stanford.edu>
2022-12-13 12:27:05 -0800

    Add RUST_EDITOR env var in build script

--- build.rs
+++ build.rs
@@ -4,11 +4,12 @@ const JS_DIST_DIR: &str = "js/packages/quiz-embed/dist";
 
 fn main() {
   if !Path::new(JS_DIST_DIR).exists() {
-    let status = Command::new("graco")
-      .current_dir("js")
-      .arg("prepare")
-      .status()
-      .unwrap();
+    let mut cmd = Command::new("graco");
+    cmd.current_dir("js").arg("prepare");
+    if cfg!(feature = "rust-editor") {
+      cmd.env("RUST_EDITOR", "yes");
+    }
+    let status = cmd.status().unwrap();
     if !status.success() {
       panic!("Failed to install/build JS")
     }
