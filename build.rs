a2d424d7c5923ccba72c5700aed1ee9f857c4692
Will Crichton <wcrichto@cs.stanford.edu>
2022-12-14 16:51:16 -0800

    Show build errors if JS fails

--- build.rs
+++ build.rs
@@ -3,14 +3,15 @@ use std::{path::Path, process::Command};
 const JS_DIST_DIR: &str = "js/packages/quiz-embed/dist";
 
 fn main() {
-  if !Path::new(JS_DIST_DIR).exists() {
+  let js_dist_dir = Path::new(JS_DIST_DIR);
+  if js_dist_dir.exists() {
     let mut cmd = Command::new("graco");
     cmd.current_dir("js").arg("prepare");
     if cfg!(feature = "rust-editor") {
       cmd.env("RUST_EDITOR", "yes");
     }
     let status = cmd.status().unwrap();
-    if !status.success() {
+    if !status.success() || !js_dist_dir.exists() {
       panic!("Failed to install/build JS")
     }
   }
