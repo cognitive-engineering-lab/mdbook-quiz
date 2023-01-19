fdbc53a03236766f650bd0dd85f10e86259e3fdd
Will Crichton <wcrichto@cs.stanford.edu>
2023-01-18 19:07:20 -0800

    Keep lineNumber in Tracing schema to prevent schema validation errors

--- js/packages/quiz-schema/src/tracing.ts
+++ js/packages/quiz-schema/src/tracing.ts
@@ -12,8 +12,8 @@ export interface TracingAnswer {
   /** If doesCompile=true, then the contents of stdout after running the program */
   stdout?: string;
 
-  // /** If doesCompile=false, then the line number of the code causing the error */
-  // lineNumber?: number;
+  /** If doesCompile=false, then the line number of the code causing the error */
+  lineNumber?: number;
 }
 
 export type Tracing = QuestionFields<"Tracing", TracingPrompt, TracingAnswer>;
