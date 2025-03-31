f4dccb591ce7e9445677c07c148bc455025dafbf
Will Crichton <wcrichto@cs.stanford.edu>
2025-03-30 21:26:05 -0700

    Replace @iarna/toml with smol-toml in the example.

--- example/standalone/src/index.tsx
+++ example/standalone/src/index.tsx
@@ -1,13 +1,13 @@
-import TOML from "@iarna/toml";
 import { type Quiz, QuizView } from "@wcrichto/quiz";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import toml from "smol-toml";
 
 import quizStr from "./quiz-example.toml?raw";
 import "./telemetry";
 
 let App = () => {
-  let quiz = TOML.parse(quizStr) as any as Quiz;
+  let quiz = toml.parse(quizStr) as any as Quiz;
   return (
     <div>
       <h1>Example quiz</h1>
