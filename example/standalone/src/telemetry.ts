97df6016b4d4560d6acca93fb942c07d163a1503
Will Crichton <wcrichto@cs.stanford.edu>
2026-05-02 23:51:55 -0400

    Roll back to simpler example

--- example/standalone/src/telemetry.ts
+++ example/standalone/src/telemetry.ts
@@ -1,38 +1,20 @@
-import * as uuid from "uuid";
-
-const TELEMETRY_URL = "https://rust-book.willcrichton.net/logs";
-
-function getSessionId() {
-  const SESSION_STORAGE_KEY = "__telemetry_session";
-  if (localStorage.getItem(SESSION_STORAGE_KEY) === null) {
-    localStorage.setItem(SESSION_STORAGE_KEY, uuid.v4());
-  }
-  return localStorage.getItem(SESSION_STORAGE_KEY)!;
-}
+const BASE_URL = "http://localhost:8888";
 
 class Telemetry {
-  private sessionId: string;
+  async log(endpoint: string, payload: object) {
+    let url = `${BASE_URL}/${endpoint}`;
 
-  constructor() {
-    this.sessionId = getSessionId();
-  }
-
-  // biome-ignore lint/suspicious/noExplicitAny: payload can be anything
-  log(_endpoint: string, payload: any) {
-    let log = {
-      sessionId: this.sessionId,
-      timestamp: new Date().getTime(),
-      payload
-    };
-
-    let fullUrl = `${TELEMETRY_URL}/rq_answers`;
-    fetch(fullUrl, {
+    let response = await fetch(url, {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
       },
-      body: JSON.stringify(log)
+      body: JSON.stringify(payload)
     });
+
+    if (!response.ok) {
+      throw new Error(`Failed to log with status: ${response.statusText} `);
+    }
   }
 }
 
--- example/standalone/vite.config.ts
+++ example/standalone/vite.config.ts
@@ -1,20 +1,7 @@
-import path from "node:path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig(() => ({
   base: "./",
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@wcrichto/rust-editor/dist/lib.css": path.resolve(
-        __dirname,
-        "rust-editor-placeholder.css"
-      ),
-      "@wcrichto/rust-editor": path.resolve(
-        __dirname,
-        "rust-editor-placeholder.js"
-      )
-    }
-  }
+  plugins: [react()]
 }));
